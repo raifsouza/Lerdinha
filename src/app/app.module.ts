267ed4b4807b4d481fb43870f266cc0fd1b67566
180ml-coffee <lemoswebston@gmail.com>
2025-03-26 00:57:27 -0300

    inclusão do modal de termos e condições, validação de cpf, data de nascimento e situação cadastral

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+<<<<<<< Updated upstream
 import { ReactiveFormsModule } from '@angular/forms';
 import { CadastroComponent } from './pages/cadastro/cadastro.component';
 import { CommonModule } from '@angular/common';
+=======
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
+
+>>>>>>> Stashed changes
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
--- src/app/pages/cadastro/cadastro.component.ts
+++ src/app/pages/cadastro/cadastro.component.ts
@@ -2,10 +2,18 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+<<<<<<< Updated upstream
+=======
+
+import { CpfValidationService } from '../../services/cpf-validation.service';
+import { provideNgxMask, NgxMaskDirective } from 'ngx-mask';
+import { LucideAngularModule, Eye, EyeOff } from 'lucide-angular';
+>>>>>>> Stashed changes
 
 @Component({
   selector: 'app-cadastro',
   standalone: true,
+<<<<<<< Updated upstream
   imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './cadastro.component.html',
   styleUrls: ['./cadastro.component.scss'],
@@ -31,6 +39,82 @@ export class CadastroComponent implements OnInit {
         password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(30)]],
         confirmPassword: ['', Validators.required],
         cep: ['', Validators.pattern(/^\d{5}-\d{3}$/)],
+=======
+  imports: [CommonModule, ReactiveFormsModule, NgxMaskDirective, LucideAngularModule],
+  templateUrl: './cadastro.component.html',
+  styleUrls: ['./cadastro.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  providers: [provideNgxMask({
+      dropSpecialCharacters: false,
+      validation: true,
+    })
+  ],
+})
+
+export class CadastroComponent implements OnInit {
+  cadastroForm!: FormGroup;
+  showPassword: boolean = false;
+  showConfirmPassword: boolean = false;
+  cpfStatus: string | null = null;
+  mostrarModal = false;
+
+  abrirModal(event: Event) {
+    event.preventDefault();
+    this.mostrarModal = true;
+  }
+
+  fecharModal() {
+    this.mostrarModal = false;
+  }
+
+  icons = { Eye, EyeOff };
+
+  constructor(private fb: FormBuilder, private CpfValidationService: CpfValidationService) {
+    this.cadastroForm = this.fb.group({
+      cpf: ['', [Validators.required, Validators.pattern(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/)]],
+      dataNascimento: ['', [Validators.required]],
+
+    })
+
+  }
+
+  async validarCpf(): Promise<void> {
+    const cpf = this.cadastroForm.get('cpf')?.value;
+    if (!cpf) return;
+
+    const isValid = await this.CpfValidationService.validarCpf(cpf);
+
+    if (!isValid) {
+      this.cadastroForm.get('cpf')?.setErrors({ invalidCpf: true });
+    }
+  }
+
+  validarDataNascimento(): void {
+    const dataNascimento = this.cadastroForm.get('dataNascimento')?.value;
+    if (!dataNascimento) return;
+
+    const dataNascimentoDate = new Date(dataNascimento);
+    const hoje = new Date();
+    const idade = hoje.getFullYear() - dataNascimentoDate.getFullYear();
+
+    if (idade < 18) {
+      this.cadastroForm.get('dataNascimento')?.setErrors({ underAge: true });
+    }
+  }
+
+  ngOnInit(): void {
+    this.cadastroForm = this.fb.group(
+      {
+        firstName: ['', [Validators.required, Validators.minLength(2)]],
+        lastName: ['', [Validators.required, Validators.minLength(2)]],
+        cpf: ['', Validators.required],
+        gender: ['', Validators.required],
+        phone: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(30)]],
+        confirmPassword: ['', Validators.required],
+        cep: ['', Validators.pattern],
+>>>>>>> Stashed changes
         terms: [false, Validators.requiredTrue]
       },
       { validators: this.passwordMatchValidator }
@@ -51,6 +135,7 @@ export class CadastroComponent implements OnInit {
     this.showConfirmPassword = !this.showConfirmPassword;
   }
 
+<<<<<<< Updated upstream
   isFieldInvalid(field: string): boolean {
     const control = this.registerForm.get(field);
     return !!control?.invalid && (control?.touched || control?.dirty);
@@ -63,6 +148,15 @@ export class CadastroComponent implements OnInit {
     } else {
       Object.keys(this.registerForm.controls).forEach(key => {
         this.registerForm.get(key)?.markAsTouched();
+=======
+  onSubmit(): void {
+    if (this.cadastroForm.valid) {
+      console.log('Form submitted:', this.cadastroForm.value);
+      alert('Cadastro realizado com sucesso!');
+    } else {
+      Object.keys(this.cadastroForm.controls).forEach(key => {
+        this.cadastroForm.get(key)?.markAsTouched();
+>>>>>>> Stashed changes
       });
     }
   }
